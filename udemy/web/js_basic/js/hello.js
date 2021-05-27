a27bf19e275d1be339db148d53b619e4378c77f0
MasakoIdei <tarte.groseilles@gmail.com>
2021-05-27 09:48:41 +0900

    [add]エラーの確認方法とコメントアウトについて

--- udemy/web/js_basic/js/hello.js
+++ udemy/web/js_basic/js/hello.js
@@ -1,1 +1,8 @@
+// コメントアウトの仕方一行の時はこの文章みたいに//を先頭につける
+// console.log('Hello World! form hello.js');
+
+/*
+コメントアウトしたい行が複数行ある時は、この行みたいにスラッシュとアスタリスクで挟みこむ
 console.log('Hello World! form hello.js');
+console.log('Hello World! form hello.js');
+*/
